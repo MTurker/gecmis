84021fba27f5727577da91ba40dd6c5d37678876
xtheory <mtf_4040@hotmail.com>
2016-05-26 18:30:24 +0300

    Veri Tabanı Bağlantısı Sağlandı

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
     $user = "root";
     $pass = "";
 
-    mysql_connect($host, $user, $pass)or die(mysql_error("Veri Tabanına Bağlanılamadı : "));
-    mysql_select_db("gecmis")or die(mysql_error("Veri tababı Seçilemedi : "));
+    mysql_connect($host, $user, $pass)or die(mysql_error());
+    mysql_select_db("gecmis")or die(mysql_error());
+    mysql_query("SET CHARACKTER NAMES utf8");
+    mysql_query("SET NAMES utf8");
 ?>
+<html>
+<head>
+    <title>Mehmet TÜRKER - Geçmiş Uygulaması</title>
+</head>
+</html>
