3be8877a12966baf245d6e4e9dd9abb3982fd8af
xtheory <mtf_4040@hotmail.com>
2016-05-26 18:43:39 +0300

    Veri Tabanı Bağlantısı Sağlandı. Türkçe karakter Sorunu Düzeltildi. 5 li Link Yapısı Oluştruldu. Sayfa Adresi Değişkene Atandı. Kayıt İşlemi Yapıldı.

--- index.php
+++ index.php
@@ -7,9 +7,27 @@
     mysql_select_db("gecmis")or die(mysql_error());
     mysql_query("SET CHARACKTER NAMES utf8");
     mysql_query("SET NAMES utf8");
+
+    $uye = 2;
+
+    $url = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 ?>
 <html>
 <head>
     <title>Mehmet TÜRKER - Geçmiş Uygulaması</title>
 </head>
+<body>
+    <a href="index.php?sayfa=1">Sayfa 1</a>
+    <a href="index.php?sayfa=2">Sayfa 2</a>
+    <a href="index.php?sayfa=3">Sayfa 3</a>
+    <a href="index.php?sayfa=4">Sayfa 4</a>
+    <a href="index.php?sayfa=5">Sayfa 5</a>
+
+<?php
+    $giris = mysql_query("INSERT INTO gezinme (id, sayfaUrl, uyeId) VALUES (NULL, '$url', '$uye')");
+    if($giris){
+        echo "Kayıt Edildi.";
+    }
+?>
+</body>
 </html>
